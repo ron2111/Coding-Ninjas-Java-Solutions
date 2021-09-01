60b9881dbb81bedb751c57f37abb306a4714868f
Rohan Sharma <70716534+ron2111@users.noreply.github.com>
2021-09-02 02:21:40 +0530

    Another simpler approach
    This is another approach to the given problem but with no other methods like children[i], get(), etc., as prescribed in the problem.
    The code is well commented to be understood easily.

--- Trees/ReplaceNodeWithDepth.java
+++ Trees/ReplaceNodeWithDepth.java
@@ -1,24 +1,29 @@
 
 public class ReplaceNodeWithDepth {
-
-	public static void replaceWithDepthValue(TreeNode<Integer> root){
-
-		// Write your code here
-		replaceWithDepthValue(root, 0);
-		
-	}
-	public static void replaceWithDepthValue(TreeNode<Integer> root,int depth){
-
-		// Write your code here
-		if(root==null) {
-			return ;
-		}
-		root.data = depth;
-		for(int i=0;i<root.children.size();i++) {
-			replaceWithDepthValue(root.children.get(i),depth+1);
-		}
-		
-		
-	}
+ //Using helper function to use depth as another arguement
+public static void changeToDepthTree(BinaryTreeNode<Integer> root) {
 	
+        helper(root,0);
+	}
+    
+    public static void helper(BinaryTreeNode<Integer> root, int k)
+    {
+        if (root==null)
+        {                   //Base Case
+            return;
+        }
+        root.data=k;  //Changing the value
+        helper(root.left,++k); //Using the pre-increment operator 
+        
+        helper(root.right,k--);// Very Important step
+        
+        /* Here k is post decremented because then the value of k is given to
+         a particular subtree's left node, it will be same for right node and
+             after changing the value of bth the nodes(if they are not null), the
+             recursion call returns to the previous call to complete the leftover commands
+             where the k is already decremented due to the post decrement operator */
+	}
+
 }
+	
+
